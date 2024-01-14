f50e26fac9304f6cc397032b6e997863caac5c11
Elias Skogevall <elias.skogevall@gmail.com>
2024-01-14 15:57:29 +0100

    Fix release script

--- scripts/release.js
+++ scripts/release.js
@@ -6,3 +6,4 @@ await execa("npm", ["version", bump], { stdio: "inherit" });
 await execa("npm", ["run", "build"], { stdio: "inherit" });
 await execa("npm", ["run", "test"], { stdio: "inherit" });
 await execa("npm", ["publish"], { stdio: "inherit", cwd: "dist" });
+await execa("git", ["push", "--tags"], { stdio: "inherit" });
